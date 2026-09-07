ebff24289ea0e661bad5fdf8a6fd46dfc8e6bf67
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-07 15:42:07 +0700

    update code

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BlogApp());
+}
+
+class BlogApp extends StatelessWidget {
+  const BlogApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Blog App',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blog App'),
+      ),
+      body: const Center(
+        child: Text(
+          'Belum ada artikel',
+        ),
+      ),
+    );
+  }
+}

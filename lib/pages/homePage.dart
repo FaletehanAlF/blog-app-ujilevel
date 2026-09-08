32cd79c034fa1d06becb594b46b490498c6f9313
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-08 10:06:27 +0700

    update code

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class HomePageextends StatefulWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
@@ -11,35 +11,6 @@ class HomePageextends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List product = [];
-
-  Future<void> getProduct() async {
-    final response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
-    if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      throw Exception('Failed to load products');
-    }
-  }
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product List'),
-      ),
-      body: ListView.builder(
-        itemCount: product.length,
-        itemBuilder: (context, index) {
-          final productItem = product[index];
-          return ListTile(
-            title: Text(productItem['name']),
-            subtitle: Text(productItem['description']),
-          );
-        },
-      ),
-    );
-  }
 
   Future<void> fetchPosts() async {
     final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
@@ -59,18 +30,21 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
-  widget.build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product List'),
+      ),
       body: ListView.builder(
         itemCount: product.length,
         itemBuilder: (context, index) {
           final itemProduct = product[index];
-         return ListTile(
-          leading: Image.network(itemProduct['image']),
+          return ListTile(
             title: Text(itemProduct['title']),
-            subtitle: Text(itemProduct['Price'].toString()),
+            subtitle: Text(itemProduct['body']),
           );
         },
-      )
+      ),
     );
   }
+}

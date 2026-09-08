fb27345d68d001b74aedc4fd576beae366728e9d
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-08 09:39:18 +0700

    update code

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -12,6 +12,17 @@ class HomePageextends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List product = [];
 
+  future<void> getProduct() async {
+    final response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    if (response.statusCode == 200) {
+      setState(() {
+        product = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
